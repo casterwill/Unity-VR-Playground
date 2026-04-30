d6da4c10fed3c70dfb946f04dd8622582ff4d560
Ahmad_Izzuddin <122771261+casterwill@users.noreply.github.com>
2026-04-30 15:13:02 +0700

    feat : adjusting minor detail
    - reduce bullet hole size
    - add iron sight
    - change bullet hole instant vanish to fade out vanish

--- Assets/EffectExamples/Shared/Scripts/DecalDestroyer.cs
+++ Assets/EffectExamples/Shared/Scripts/DecalDestroyer.cs
@@ -6,9 +6,26 @@ public class DecalDestroyer : MonoBehaviour {
 
 	public float lifeTime = 5.0f;
 
+	[SerializeField] GameObject holeDecals;
+    [SerializeField] float fadeOutDuration = 2f;
+
 	private IEnumerator Start()
 	{
 		yield return new WaitForSeconds(lifeTime);
-		Destroy(gameObject);
+
+        Material holeMaterial = holeDecals.GetComponent<MeshRenderer>().material;
+
+        float startAlpha = holeMaterial.color.a;
+        for (float t = 0f; t < fadeOutDuration; t += Time.deltaTime)
+        {
+            float normalizedTime = t / fadeOutDuration;
+            Color newColor = holeMaterial.color;
+            newColor.a = Mathf.Lerp(startAlpha, 0f, normalizedTime);
+            holeMaterial.color = newColor;
+            yield return null;
+        }
+        // Ensure it ends at exactly 0
+        holeMaterial.color = new Color(holeMaterial.color.r, holeMaterial.color.g, holeMaterial.color.b, 0f);
+        Destroy(gameObject);
 	}
 }
